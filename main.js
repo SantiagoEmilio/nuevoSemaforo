6ee3de3db461a60a5194eac60c05bcf85e67e549
Anonimo <anonimo@example.com>
2025-07-18 12:50:53 -0600

    se actualiaaa

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 
-// Configuración Firebase
+// 🔧 Nueva configuración Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDZTRFG7yBzizPSJJImWddGwbyQiWQKGH8",
-  authDomain: "raspberry-pi-pico-w-5635e.firebaseapp.com",
-  databaseURL: "https://raspberry-pi-pico-w-5635e-default-rtdb.firebaseio.com",
-  projectId: "raspberry-pi-pico-w-5635e",
+  authDomain: "raspberry-pi-pico-w-5635e.firebaseapp.com",  // Puedes dejarlo igual si no lo necesitas
+  databaseURL: "https://semaforooo-3314e-default-rtdb.firebaseio.com", // 🔁 Cambiado aquí
+  projectId: "raspberry-pi-pico-w-5635e", // No afecta la base de datos RTDB
   storageBucket: "raspberry-pi-pico-w-5635e.appspot.com",
   messagingSenderId: "5310962444",
   appId: "1:5310962444:web:ac110a2284b6e138694729"
@@ -15,7 +15,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-const semaforoRef = ref(db, 'semaforo');
+const semaforoRef = ref(db, 'semaforo/estado_actual');
 
 const luces = {
   rojo: document.getElementById("rojo"),
@@ -41,6 +41,8 @@ onValue(semaforoRef, (snapshot) => {
   const data = snapshot.val();
   if (data && data.estado) {
     actualizarVista(data.estado);
+  } else if (data && data.color) {
+    actualizarVista(data.color); // ✅ para cuando se envía "color" en lugar de "estado"
   } else {
     label.textContent = "Esperando conexión con Firebase...";
   }
